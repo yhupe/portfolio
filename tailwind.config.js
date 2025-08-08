14ba31638f0412ecd0473dc9de060b6c6de7f32e
yhupe <hannes.pickel@outlook.com>
2025-08-08 18:01:40 +0200

    correct tailwind v4 setup, cleaning up input.css, tailwind.config.css and html templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,5 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./app/public/templates/**/*.html", "./app/**/*.py"],
-  safelist: [
-    'bg-ink-900', 'bg-ink-800', 'text-paper-100'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        ink:   { 900: "#0b0d10", 800: "#12161b" },
-        paper: { 100: "#e6e9ef", 50: "#f2f4f8" },
-        accent:{ mint:"#9be7c4", peach:"#ffc6a5", sky:"#9cc9ff", lilac:"#c7b7ff" },
-      },
-    },
-  },
-  plugins: [],
-}
+};
